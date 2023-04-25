b0c3e7e276b15c3db784ca615ee773f6d2d42a19
Irbisk <Irbis2496@ya.ru>
2023-04-25 13:13:31 +0300

    Stage 2

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -1,8 +1,18 @@
 package cinema
 
+
+const val FREE_SEAT = 'S'
+
 fun main() {
-    val grid = MutableList(7) { MutableList(8) { 'S' } }
-    printTheatre(grid)
+    println("Enter the number of rows:")
+    val rows = readln().toInt()
+    println("Enter the number of seats in each row:")
+    val seatsInRows = readln().toInt()
+    val grid = MutableList(rows) { MutableList(seatsInRows) { FREE_SEAT } }
+
+    println("Total income:")
+    println("$${countTotalIncome(grid)}")
+
 }
 
 fun printTheatre(grid: MutableList<MutableList<Char>>) {
@@ -11,4 +21,17 @@ fun printTheatre(grid: MutableList<MutableList<Char>>) {
     for (i in grid.indices) {
         println("${i + 1} ${grid[i].joinToString(" ")} ")
     }
+}
+
+fun countTotalIncome(grid: MutableList<MutableList<Char>>): Int {
+    var totalIncome = 0
+    val totalSeats = grid.size * grid[0].size
+    totalIncome = if (totalSeats < 60) {
+        totalSeats * 10
+    } else {
+        val firstPart = grid.size / 2
+        val secondPart = grid.size - firstPart
+        (firstPart * 10 + secondPart * 8) * grid[0].size
+    }
+    return totalIncome
 }
